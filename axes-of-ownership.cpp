3acf5579aedfa73c5ba4077eb5dcdc14498ee5f9
Jay Krell <jay.krell@cornell.edu>
2023-01-28 15:01:28 -0800

    v2

--- axes-of-ownership.cpp
+++ axes-of-ownership.cpp
@@ -1,10 +1,13 @@
 /*
 Ownership can be unique or shared.
 Ownership can be constant or changing.
-Changes in ownership can be lockfree or locked or single threaded.
+Changes in ownership can be atomic or not.
+  Atomic can be lockfree or locked.
+  For example, single threaded ownership changes do not need to be atomic.
 
 Ownership is usually unique and constant. Local variables.
 Single threaded shared changing ownership is for example passing the address of local variables, with LIFO order.
 Ownership can be shared and constant. Global constants.
 */
 
+// TODO: Examples?
